becknik/dotfiles.nix

commit 249bd66441cdd48d45ea6647802b6f86573819ff

home-manager: change git secret naming & adjust sopra/est ssh key

home-manager/secrets.nix
@@ -42,7 +42,7 @@ in
       "github-personal.pub" = {
         sopsFile = ./secrets/git.yaml;
         format = "yaml";
-        key = "github/personal/pub.ed25519"; # "No tested [yaml]-data structures [apart from strings] are supported right now" - including directories?!
+        key = "github/personal/ed25519.pub"; # "No tested [yaml]-data structures [apart from strings] are supported right now" - including directories?!
         mode = "0400";
         /*symlink-*/ path = "${config.home.homeDirectory}/.ssh/github-personal.pub";
       };
@@ -56,16 +56,16 @@ in
         path = "${config.home.homeDirectory}/.ssh/github-personal";
       };
 
-      "gitlab-sopra.pub" = {
+      "gitlab-est.pub" = {
         sopsFile = ./secrets/git.yaml;
-        key = "gitlab/sopra/pub.ed25519";
+        key = "gitlab/est/ed25519.pub";
         mode = "0400";
-        /*symlink-*/ path = "${config.home.homeDirectory}/.ssh/gitlab-sopra.pub";
+        path = "${config.home.homeDirectory}/.ssh/gitlab-est.pub";
       };
-      "gitlab-sopra" = {
+      "gitlab-est" = {
         sopsFile = ./secrets/git.yaml;
-        key = "gitlab/sopra/.ed25519";
-        path = "${config.home.homeDirectory}/.ssh/gitlab-sopra";
+        key = "gitlab/est/.ed25519";
+        path = "${config.home.homeDirectory}/.ssh/gitlab-est";
       };
 
       # Creation of secrets from binaries: `sops -e ./desktop-env/secrets/keepass.key > ./desktop-env/secrets/keepassxc.yaml`

home-manager/secrets/git.yaml
@@ -1,10 +1,10 @@
 github:
     personal:
-        pub.ed25519: ENC[AES256_GCM,data:8z6YoC9UjA0cnbXZBirM8mlzEoPMAGA3AjqKwngykqztlcqRAxZPReEQxjdR50Qbyq0zR+11j/jzYRtbUT75fiNb9qH+/36Vme6DibuFUltDuRFCJg5z9MpqkvFOdJXlWnM=,iv:j5AsZIrVfYwidj+FxNfecx7IBOw6KrZ0OyfKc0o93WI=,tag:sV2OYXCPkH3nQpn+usLe8w==,type:str]
+        ed25519.pub: ENC[AES256_GCM,data:8z6YoC9UjA0cnbXZBirM8mlzEoPMAGA3AjqKwngykqztlcqRAxZPReEQxjdR50Qbyq0zR+11j/jzYRtbUT75fiNb9qH+/36Vme6DibuFUltDuRFCJg5z9MpqkvFOdJXlWnM=,iv:j5AsZIrVfYwidj+FxNfecx7IBOw6KrZ0OyfKc0o93WI=,tag:sV2OYXCPkH3nQpn+usLe8w==,type:str]
         .ed25519: ENC[AES256_GCM,data:0aX9eQxbHTGn5W/XztrOUmtxs7EhlhG4Plvw3RpAwp6ocF8AhRiXmFsXEeFA26u00xVlqkyFty5sibCixPE29YhsyIpP+Wyc5DtfnfFKOXzuojWimfpf5upyLxioJzmZwMFmRwCwmDGWjKc782XudDiM9JKVQIO+Lhu4v4eaNjWBaccTv6z2m+QI8fHgTHHBZGoJJudMSZCf5tYSenbsAWOINKdrG7wi9T20eZKwDV8bPLDtDpZKc3CXFhkakGpmxlZ6+9MQykT/5CHxxYXIIP0aiSEM1Q+bBIBHCXFrgmeMWv0O+zultXAPTou5dKZSMsRzSp2cIuyH351IH2yWMIfl42KSjv36SHypWhR9lQfYdbeb+QE4RUsNN5+LXfpIck+nUQA0bjCIiQ9+aeMpMGi6kCkzlB0Do4GuFIQQVISSL+bHbUSt1+Brx+ov2TAx8acxLwd28P/GVr9IijrSnw9D7jjPIhNvB8nM9rmpk76AJtwen7N3sNxstJ5u/LXTRzANV3oW/maX+oKy3fg2+cGi0A8iY6xuCX4PXOosodEj8AoiaGhfzkwGsV0GSrkv2qE/t9Bn+Nvm6uLTbA+/nVrfjuPNOI/CLGGqXEg0o84=,iv:q9R2mfalFcZy8rBe5/kqiw47IkHUq+r2DCq+D06k/00=,tag:oD45NvpsRMmFuKiAoAKumw==,type:str]
 gitlab:
-    sopra:
-        pub.ed25519: ENC[AES256_GCM,data:iTsgiqcCABrejz2+tU/Qc2Z84NvCX79Kg9yVjh7iud57vcloF0Eg5v5NLiLASXUJQhGTSwmffT1disb5EJqqjRYfY+7W6Ev016FMweq/Wxrus+YRbdCqbjef,iv:GZbyWsOquv7ikc4Mt70s36XzDatkTay52Ecu6/U9aps=,tag:nPWELEmtImF0zQHRxArLmQ==,type:str]
+    est:
+        ed25519.pub: ENC[AES256_GCM,data:iTsgiqcCABrejz2+tU/Qc2Z84NvCX79Kg9yVjh7iud57vcloF0Eg5v5NLiLASXUJQhGTSwmffT1disb5EJqqjRYfY+7W6Ev016FMweq/Wxrus+YRbdCqbjef,iv:GZbyWsOquv7ikc4Mt70s36XzDatkTay52Ecu6/U9aps=,tag:nPWELEmtImF0zQHRxArLmQ==,type:str]
         .ed25519: ENC[AES256_GCM,data:Vn3WGguHCyY+l1vDBRKmdfggrJQJAWHoBaa2cP92fUROrQ6ubF3i3W56wL60xx/+hoVPsLKJu/lsUGdLU24Aplphb5Cho7uVVFMYRktIcAosJNnQUFdkVmT/a4n+FpHLOa0/NLJK3IgPoOD3UzN7LxXz9/LmIFtN0SFJc8sfrg4CO89Rzaa46gdOPxv3bhCInnicqVOfP8T7LiShu+n1SwdNqlKzEbpzbf9QWtUTj6iBNBGfPjswS646EUFem0cD0B2ZFmsMKLKHaW+BU43E1kE4d8rspJbZQeLEinNkhiVroBzwFK3+/jT6FotCfOAUnf77F94k7uJ3R2MyzYa0stxVpOmYKPlSYzxk8u699NmD8cF8sriWgXrfB2J/kCbHm5LItFl5zcQuEp40G7rYU7WEUPc4tUD6n0KZFd4jdBaK2FjYRQw3y08kYE8cPVEdPxdGZqJk9MqYXkBPm6bZZ5MZM/HSDlDFbKGs50wmidB4o028ZegLM0m2ChlEc9755J8lc7zxOsVJP/4CS5dfOmmbI/Zm+5Huz6qaEZkF7KWMfEmwKO/MUU9NIJyge6X0FyNb0zoeoBbcoN0u,iv:+T77GKuTz/d8o9hPW/PUZscJYgE573C6/oaQ+pkDnxA=,tag:4iLpb8AxMifbBzVgUpo56Q==,type:str]
 sops:
     kms: []

home-manager/devel.nix
@@ -36,9 +36,9 @@
           identityFile = "~/.ssh/github-personal";
         };
         gitlab_sopra = {
-          host = "sopra.informatik.uni-stuttgart.de";
+          host = "gila24.informatik.uni-stuttgart.de";
           user = "git";
-          identityFile = "~/.ssh/gitlab-sopra";
+          identityFile = "~/.ssh/gitlab-est";
           addressFamily = "inet";
         };
       };

commit 34136e2c55b818108428fe5980d709b358b38b52

fix(home-manager): secrets broken after 249bd66

home-manager/secrets.nix
@@ -43,7 +43,6 @@ in
         sopsFile = ./secrets/git.yaml;
         format = "yaml";
         key = "github/personal/ed25519.pub"; # "No tested [yaml]-data structures [apart from strings] are supported right now" - including directories?!
-        mode = "0400";
         /*symlink-*/ path = "${config.home.homeDirectory}/.ssh/github-personal.pub";
       };
 
@@ -52,19 +51,18 @@ in
       # TODO secrets are symlinked twice?!
       "github-personal" = {
         sopsFile = ./secrets/git.yaml;
-        key = "github/personal/.ed25519";
+        key = "github/personal/ed25519";
         path = "${config.home.homeDirectory}/.ssh/github-personal";
       };
 
       "gitlab-est.pub" = {
         sopsFile = ./secrets/git.yaml;
         key = "gitlab/est/ed25519.pub";
-        mode = "0400";
         path = "${config.home.homeDirectory}/.ssh/gitlab-est.pub";
       };
       "gitlab-est" = {
         sopsFile = ./secrets/git.yaml;
-        key = "gitlab/est/.ed25519";
+        key = "gitlab/est/ed25519";
         path = "${config.home.homeDirectory}/.ssh/gitlab-est";
       };
 

home-manager/secrets/git.yaml
@@ -1,11 +1,11 @@
 github:
     personal:
-        ed25519.pub: ENC[AES256_GCM,data:8z6YoC9UjA0cnbXZBirM8mlzEoPMAGA3AjqKwngykqztlcqRAxZPReEQxjdR50Qbyq0zR+11j/jzYRtbUT75fiNb9qH+/36Vme6DibuFUltDuRFCJg5z9MpqkvFOdJXlWnM=,iv:j5AsZIrVfYwidj+FxNfecx7IBOw6KrZ0OyfKc0o93WI=,tag:sV2OYXCPkH3nQpn+usLe8w==,type:str]
-        .ed25519: ENC[AES256_GCM,data:0aX9eQxbHTGn5W/XztrOUmtxs7EhlhG4Plvw3RpAwp6ocF8AhRiXmFsXEeFA26u00xVlqkyFty5sibCixPE29YhsyIpP+Wyc5DtfnfFKOXzuojWimfpf5upyLxioJzmZwMFmRwCwmDGWjKc782XudDiM9JKVQIO+Lhu4v4eaNjWBaccTv6z2m+QI8fHgTHHBZGoJJudMSZCf5tYSenbsAWOINKdrG7wi9T20eZKwDV8bPLDtDpZKc3CXFhkakGpmxlZ6+9MQykT/5CHxxYXIIP0aiSEM1Q+bBIBHCXFrgmeMWv0O+zultXAPTou5dKZSMsRzSp2cIuyH351IH2yWMIfl42KSjv36SHypWhR9lQfYdbeb+QE4RUsNN5+LXfpIck+nUQA0bjCIiQ9+aeMpMGi6kCkzlB0Do4GuFIQQVISSL+bHbUSt1+Brx+ov2TAx8acxLwd28P/GVr9IijrSnw9D7jjPIhNvB8nM9rmpk76AJtwen7N3sNxstJ5u/LXTRzANV3oW/maX+oKy3fg2+cGi0A8iY6xuCX4PXOosodEj8AoiaGhfzkwGsV0GSrkv2qE/t9Bn+Nvm6uLTbA+/nVrfjuPNOI/CLGGqXEg0o84=,iv:q9R2mfalFcZy8rBe5/kqiw47IkHUq+r2DCq+D06k/00=,tag:oD45NvpsRMmFuKiAoAKumw==,type:str]
+        ed25519.pub: ENC[AES256_GCM,data:QIRbbbOsAM948+JH1gb+Ct3rgn/4EcnMKZ4YDe+9CZ6lAw/s1h/37LjxQfcOwbU/xk0ug71dnN+n9AQxZ6p7e0YBCS0dOgqwoWUbFqj9NGz+wMhXJ3VmzI9n7PhziT3oFY4=,iv:rgCY5GIpuJO8OZU4zYXUGQ4MS7ZcLGTHVxXBFoApFpE=,tag:lw9fuIm2GZrIayvmeHe3fg==,type:str]
+        ed25519: ENC[AES256_GCM,data:tdoIBELsr7kA8yx6DpZ3CPRIvas4n2jIZVICRsW4m1aG4wd6k/CuILwrjSAE2wJLOyCgU4F+2rFFu7ny3lCWJq45lW4M1b1f1SyGOTGnLgM5wW4ueA4JQigN2skcc4337Qixh8dzkgarhcysDnQCZPcrLPQ5Vb1oTo0eRzM8VFBxwAzo1kyYoAyDCg7UGEthrTH6FkBK5PZ3idP14FeBbxDuC+GaJQmsMt76ZCufHJmkva9R7hPE7kfJ3y4q3DOrA2paesSZEkeBJzw2sjlL4Lk52g6ZQeo+PB43wSFBB8NM3jn38dHW/RXzuRsINRRqXZnirZnoNkRzkUN9zvTRAcZRiKrRsfJ2z8+uuWzpzyF6t7/JD4iaUQNUDjlqKW7OwVVLtpBUg7KeJGrljLwYNFQ7qFERF9S42tBzyL66V1pmYHr1QFCOqvqELFePNZ8x+hI3fIwuza0yQTxX9kkJukOUMJnF5IMlaKJ2wfmycbuZb1LDpwKoeL4QPFfCqiSC/oYasYR8TJgK9fDvehMc2Mw/wsDOvhU2ljoIOH1omDrkXI0hxocy2u7NXqce7y+16PeKYz1iTJCZxebx9WTlxhPSPx8kfTuc0w357JeZ2wQ=,iv:aIZd7wlBBJr4NZENeyEKcH2dAm/jAtZwHv1iuevhUnw=,tag:rSG+K8+IcnZE6hsL6qTX2g==,type:str]
 gitlab:
     est:
-        ed25519.pub: ENC[AES256_GCM,data:iTsgiqcCABrejz2+tU/Qc2Z84NvCX79Kg9yVjh7iud57vcloF0Eg5v5NLiLASXUJQhGTSwmffT1disb5EJqqjRYfY+7W6Ev016FMweq/Wxrus+YRbdCqbjef,iv:GZbyWsOquv7ikc4Mt70s36XzDatkTay52Ecu6/U9aps=,tag:nPWELEmtImF0zQHRxArLmQ==,type:str]
-        .ed25519: ENC[AES256_GCM,data:Vn3WGguHCyY+l1vDBRKmdfggrJQJAWHoBaa2cP92fUROrQ6ubF3i3W56wL60xx/+hoVPsLKJu/lsUGdLU24Aplphb5Cho7uVVFMYRktIcAosJNnQUFdkVmT/a4n+FpHLOa0/NLJK3IgPoOD3UzN7LxXz9/LmIFtN0SFJc8sfrg4CO89Rzaa46gdOPxv3bhCInnicqVOfP8T7LiShu+n1SwdNqlKzEbpzbf9QWtUTj6iBNBGfPjswS646EUFem0cD0B2ZFmsMKLKHaW+BU43E1kE4d8rspJbZQeLEinNkhiVroBzwFK3+/jT6FotCfOAUnf77F94k7uJ3R2MyzYa0stxVpOmYKPlSYzxk8u699NmD8cF8sriWgXrfB2J/kCbHm5LItFl5zcQuEp40G7rYU7WEUPc4tUD6n0KZFd4jdBaK2FjYRQw3y08kYE8cPVEdPxdGZqJk9MqYXkBPm6bZZ5MZM/HSDlDFbKGs50wmidB4o028ZegLM0m2ChlEc9755J8lc7zxOsVJP/4CS5dfOmmbI/Zm+5Huz6qaEZkF7KWMfEmwKO/MUU9NIJyge6X0FyNb0zoeoBbcoN0u,iv:+T77GKuTz/d8o9hPW/PUZscJYgE573C6/oaQ+pkDnxA=,tag:4iLpb8AxMifbBzVgUpo56Q==,type:str]
+        ed25519.pub: ENC[AES256_GCM,data:5+lRhzp4qPF/VEwbJsqpMbh307zmxQumI9FK4QbAMla5wl83e5vmAm4v7daCm4u6f2SZrqkOqxHoUh6Un+pKxVQiHMwaJsC281rmFJCzTUnwsfAKL9hAhQNW,iv:h6ZGzJx5/GbGyNrl4kNm07SJXTfR3IhbDGdm5NwAUuU=,tag:8cfApUq5urACmHKurmLOSA==,type:str]
+        ed25519: ENC[AES256_GCM,data:NKaDHy4o4L7DX29jKvWqoNXFFvQhfs9iHXBCpdrXRrFW312sgHFKdUg+pFQDby60E/qHxMwplCwI14K947Bk7ykEOCQ6SXAO9HthNuQS1C0vzLTtqzXpJm6jS8pMJYPi2D/hA+fD/+PpJu62whMWiPkAGVF4MbAbEKP9eCamF2wvdxTrhsoCDspE+tY4sUgOrjsy4PPb6wkJM+ygNtgo3ZA3QpNq0RDa+mdm70H0Q/UwCPPIkH08fAHeitHMInZh8/LWHy4PLuc9/rmnsFDh4VbJjVngS0qXlnDz9Gf4AoesqzdWyfx/IBeHRaK0bjuLhvMVH/w1XUYq2yGknM2HJcnzA6Me2O+b1bHOhL1BwQLKNQMHnnxPQ+48Pykml9CKE5yUjSBHTtOeE+xOzoBwvojAaXZXiSA4ChSXp345gE67FvExtN88xfvXqWNn4BJhFculET8BP7XHeKvpmvX2e/AxQBigOlTcGT7TWmzlPOP3u+ao3Uc1zkit06buaSUqbSFx7LNyLT1JA57gDAWRUCw4pkI9fXdMOeGXwND9odTiPPL7FFakxJW05JLMEkj6n/z16+icbLKpjfRh,iv:RDH0xRf3wMBfwN8wwGgKJ8cgYDueAHd+o6x/KdFW5sA=,tag:cRXTC+dfhNffTSxmd/wjeg==,type:str]
 sops:
     kms: []
     gcp_kms: []
@@ -21,8 +21,8 @@ sops:
             bUR4MTIxeCtwVWZhMllmSlJxdDBROTAK6hgmGPpSISBR9XhtO4baPg0fLNcRxdJb
             gzu16AZnRUtYWiCcPCJLLs587QkyYYcKYsCPAr4Esyxwb0rRaHCimg==
             -----END AGE ENCRYPTED FILE-----
-    lastmodified: "2024-04-20T10:51:30Z"
-    mac: ENC[AES256_GCM,data:2BP7WeenoiIaafQCQ0ZYmjp/fulEsRtEaZKtf7kv+phr9wK+F4jPBpo+IgkGUASzq3RkOaCIX3uUajhoIehB/BuC48Tzmy0p32CmeXlVDneoi77sauqf8XwzTyhyODlyui3N2gH58YpiBW3Eh8WSyDlvDBmVhcyc965yS4bN/bE=,iv:HLdhNlZflBYht/oG2cCbXIlNlutg8sraqMU2zZm56nI=,tag:W8uzMgyMScS0WE8pYhUEag==,type:str]
+    lastmodified: "2024-05-30T17:52:27Z"
+    mac: ENC[AES256_GCM,data:NmbRR/Z2HtFHV0K5/EhZjGMB9WsHYnX1YJ3Jr48RbuSz6/sXPAGXvXF1uIYI3LU+PbjvI422hx3v3qH2pJVpBi55Ax0j/wWYDbWVOweAGtw03jE8kLvmSFuFh6IhyoDZabsbnvxtY4HeKYQOAor4YK9tkAzX0sbae5wacxfhdPE=,iv:kY0zw3pGG2o0tLxk3knGM49yMNeCnF0h2dhx3K6CDfM=,tag:x0QMPG5iBorbK2uGFfPx1A==,type:str]
     pgp: []
     unencrypted_suffix: _unencrypted
     version: 3.8.1