f4fd6b017c80c208e110ebf17b748f9dadd0b549
becknik <jannikb@posteo.de>
2024-09-29 21:42:59 +0200

    nixvim: enable performance options

--- nixvim/default.nix
+++ nixvim/default.nix
@@ -21,6 +21,16 @@
     # colorschemes.onedark.enable = true;
     # colorschemes.tokyonight.enable = true;
 
+    performance = {
+      byteCompileLua = {
+        enable = true;
+        # configs = true;
+        # initLua = true;
+        nvimRuntime = false;
+        plugins = true; # some issues with some plugins like neoclip and sqlite
+      };
+    };
+
     # plugins.lazy.enable = true;
   };
 }
