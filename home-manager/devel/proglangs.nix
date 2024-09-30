654ec25028b844492db2e674d74a8a6919279749
becknik <jannikb@posteo.de>
2024-10-01 00:52:41 +0200

    home-manager: remove `minor.patch` from jdks in `~/.jdks`; add latest

--- home-manager/devel/proglangs.nix
+++ home-manager/devel/proglangs.nix
@@ -29,8 +29,8 @@
   # Kudos to @TLATER https://discourse.nixos.org/t/nix-language-question-linking-a-list-of-packages-to-home-files/38520
   home.file = (builtins.listToAttrs (builtins.map
     (jdk: {
-      name = ".jdks/jdk-${jdk.version}";
+      name = ".jdks/jdk-${builtins.elemAt (builtins.splitVersion jdk.version) 0}";
       value = { source = jdk; };
     })
-    (with pkgs.unstable; [ temurin-bin-17 temurin-bin-21 ])));
+    (with pkgs.unstable; [ temurin-bin-17 temurin-bin-21 temurin-bin ])));
 }
