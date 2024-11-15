cbd171397b8f08320220a20be0b02430000da0a1
becknik <jannikb@posteo.de>
2024-11-16 00:34:32 +0100

    home-manager: disable YSU hard mode & add better aliases for git submodule

--- home-manager/desktop-env/files/.zshrc.initExtra.zsh
+++ home-manager/desktop-env/files/.zshrc.initExtra.zsh
@@ -19,7 +19,8 @@ bindkey '^[[A' history-substring-search-up
 bindkey '^[[B' history-substring-search-down
 # you-should-use
 YSU_MESSAGE_POSITION="after"
-YSU_HARDCORE=1 # refuses to execute the command aliases exist of
+# too annoying
+YSU_HARDCORE=0 # refuses to execute the command aliases exist of
 
 # Pure Prompt
 autoload -U promptinit; promptinit
