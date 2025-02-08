9ddcad5d525760ca4ba2e29b8f0e4dfe3e17d848
becknik <jannikb@posteo.de>
2025-02-08 18:02:01 +0100

    flake: change `nix-vscode-extensions` to follow `nixpkgs`

--- flake.nix
+++ flake.nix
@@ -44,7 +44,7 @@
     };
     nix-vscode-extensions = {
       url = "github:nix-community/nix-vscode-extensions";
-      inputs.nixpkgs.follows = "nixpkgs-unstable";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     nix-index-database = {
       url = "github:nix-community/nix-index-database";
