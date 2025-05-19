67ca9935c84a27511ce9b5da0755845e269a81d8
Jannik Becker <sprinteins.becker@extaccount.com>
2025-05-19 13:51:27 +0200

    nixvim: manually source & load `~/.config/nvim` folder contents

--- nixvim/default.nix
+++ nixvim/default.nix
@@ -17,7 +17,18 @@
 
     env = { };
     files = { };
+
     impureRtp = false; # "keep .config/nvim & .config/nvim/after in runtime path"
+    # have to do this manually, maybe due to "exotic" setup using flake packages attr?
+    extraConfigLuaPre = ''
+      local config_dir = vim.fn.stdpath('config')
+      local lua_patterns = {
+        config_dir .. '/lua/?.lua',
+        config_dir .. '/lua/?/init.lua',
+      }
+      package.path = package.path .. ';' .. table.concat(lua_patterns, ';')
+      dofile(config_dir .. '/init.lua')
+    '';
 
     withPython3 = false;
     withRuby = false;
