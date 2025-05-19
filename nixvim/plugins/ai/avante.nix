81bcd8fa23ea4d79d1f2910e56d3d52c79025f53
Jannik Becker <sprinteins.becker@extaccount.com>
2025-05-19 18:21:19 +0200

    nixvim: configure avante

--- nixvim/plugins/ai/avante.nix
+++ nixvim/plugins/ai/avante.nix
@@ -7,6 +7,16 @@
     settings = {
       provider = "copilot";
       auto_suggestions_provider = "copilot";
+
+      windows = {
+        width = 35; # %
+        sidebar_header.enabled = false;
+        input.height = 12;
+      };
+
+      mappings = {
+        sidebar.add_file = "f"; # for "find"
+      };
     };
   };
 
