8148a3fc8f2b6b97081577d2bd60e2babaec073f
Jannik Becker <sprinteins.becker@extaccount.com>
2025-05-21 13:42:09 +0200

    nixvim: add keybindings for typescript-tools actions (and fix lsp setup)

--- nixvim/plugins/languages/default.nix
+++ nixvim/plugins/languages/default.nix
@@ -1,10 +1,11 @@
-{ config, lib, ... }:
+{ ... }:
 
 {
   imports = [
     ./markdown.nix
     ./nix.nix
     ./latex.nix
+    ./typescript-tools.nix
   ];
 
   # 󰲒 󰢱
@@ -13,17 +14,4 @@
       { "<leader>l", icon = "󰛦 ", desc = "Language-specific" },
     }
   '';
-
-  plugins.typescript-tools = {
-    enable = true;
-
-    settings = {
-      server = {
-        on_attach = "_M.lspOnAttach";
-        capabilities.__raw = "vim.tbl_deep_extend('force', vim.lsp.protocol.make_client_capabilities(), require('cmp_nvim_lsp').default_capabilities())";
-        root_dir.__raw = "require('lspconfig.util').root_pattern('tsconfig.json')";
-        settings.tsserver.format.enable = false;
-      };
-    };
-  };
 }
---
+++ nixvim/plugins/languages/typescript-tools.nix
@@ -0,0 +1,62 @@
+{...}: 
+
+{
+  plugins.typescript-tools = {
+    enable = true;
+
+    settings = {
+      capabilities.__raw = "vim.tbl_deep_extend('force', vim.lsp.protocol.make_client_capabilities(), require('cmp_nvim_lsp').default_capabilities())";
+
+      on_attach.__raw = ''
+        function(client, bufnr)
+          _M.lspOnAttach(client, bufnr)
+
+          vim.keymap.set(
+            "n",
+            "<leader>ci",
+            function()
+              vim.api.nvim_command("TSToolsOrganizeImports")
+            end,
+            { buffer = bufnr, desc = "organize Imports" }
+          )
+          vim.keymap.set(
+            "n",
+            "<leader>cm",
+            function()
+              vim.api.nvim_command("TSToolsAddMissingImports")
+            end,
+            { buffer = bufnr, desc = "add Missing Imports" }
+          )
+
+          vim.keymap.set(
+            "n",
+            "<leader>cu",
+            function()
+              vim.api.nvim_command("TSToolsRemoveUnused")
+            end,
+            { buffer = bufnr, desc = "remove Unused" }
+          )
+          vim.keymap.set(
+            "n",
+            "<leader>cf",
+            function()
+              vim.api.nvim_command("TSToolsFixAll")
+            end,
+            { buffer = bufnr, desc = "Fix all" }
+          )
+          wk.add({ { "<leader>ci", icon = "󰛦 " }, }, { buffer = bufnr, })
+
+          wk.add({
+            { "<leader>ci", icon = "󰛦 " },
+            { "<leader>cm", icon = "󰛦 " },
+            { "<leader>cu", icon = "󰛦 " },
+            { "<leader>cf", icon = "󰛦 " },
+          }, { buffer = bufnr, })
+        end
+      '';
+
+      root_dir.__raw = "require('lspconfig.util').root_pattern('tsconfig.json')";
+      settings.tsserver.format.enable = false;
+    };
+  };
+}
