6b6947f609637ffb100ce95a16942b0e7e14fc6a
Jannik Becker <sprinteins.becker@extaccount.com>
2025-05-21 19:04:07 +0200

    nixvim: add diagnostic-counter-signs script

--- nixvim/scripts/default.nix
+++ nixvim/scripts/default.nix
@@ -1,7 +1,11 @@
-{ ... }:
+{ config, ... }:
 
 {
   extraFiles = {
     diagnostic-counter-signs.source = ./diagnostic-counter-signs.lua;
   };
+
+  extraConfigLua = ''
+    vim.cmd('luafile ${config.extraFiles.diagnostic-counter-signs.source}')
+  '';
 }
