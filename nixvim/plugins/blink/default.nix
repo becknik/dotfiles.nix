2616f33ffa7d012cd9c89fd4d0baf766694be9bb
becknik <becknik@pm.me>
2025-06-22 16:01:26 +0200

    nixvim: compile blink-cmp fuzzy lib with optimizations

--- nixvim/plugins/blink/default.nix
+++ nixvim/plugins/blink/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   imports = [
@@ -7,71 +7,103 @@
     ./providers.nix
   ];
 
-  plugins.blink-cmp = {
-    enable = true;
-    setupLspCapabilities = true;
+  plugins.blink-cmp =
+    let
+      blink-cmp = pkgs.vimPlugins.blink-cmp;
 
-    settings = {
-      enabled.__raw = ''
-        function()
-          return not vim.list_contains({ 'sagarename' }, vim.bo.filetype)
-            and vim.b.completion ~= false
-        end'';
+      blink-cmp-fuzzy-lib-native = blink-cmp.passthru.blink-fuzzy-lib.overrideAttrs (prev: {
+        patches = [
+          ./blink-fuzzy-optimization.patch
+        ];
 
-      appearance.nerd_font_variant = "normal";
-      appearance.use_nvim_cmp_as_default = true;
+        env = (prev.env or { }) // {
+          RUSTFLAGS = pkgs.lib.concatStringsSep " " [
+            "-C"
+            "target-cpu=native"
+          ];
+        };
+      });
+      blink-cmp-native = blink-cmp.overrideAttrs (old: {
+        preInstall =
+          let
+            ext = pkgs.stdenv.hostPlatform.extensions.sharedLibrary;
+          in
+          ''
+            mkdir -p target/release
+            ln -s ${blink-cmp-fuzzy-lib-native}/lib/libblink_cmp_fuzzy${ext} target/release/libblink_cmp_fuzzy${ext}
+          '';
+        passthru = old.passthru // {
+          blink-fuzzy-lib = blink-cmp-fuzzy-lib-native;
+        };
+      });
 
-      completion.documentation.auto_show = false;
-      completion.list.max_items = 500;
-      completion.list.selection = {
-        auto_insert = false;
-        preselect = true;
-      };
-      completion.menu.draw.treesitter = [ "lsp" ]; # what is this doing?
-      # https://cmp.saghen.dev/configuration/reference.html#completion-menu-draw
-      completion.menu.draw.components.kind_icon.text.__raw = ''
-        function(ctx)
-          -- print(vim.inspect(ctx))
-          return ctx.kind_icon .. ctx.icon_gap
-        end
-      '';
-      completion.menu.draw.__raw = ''
-        {
-          columns = {
-            { "item_idx" },
-            { "kind_icon" },
-            -- :lua print(vim.inspect(vim.lsp.get_active_clients()[1].capabilities.textDocument.completion))
-            { "label", "label_description", gap = 1 },
-          },
-          components = {
-            item_idx = {
-              text = function(ctx)
-                return ctx.idx == 10 and '0' or ctx.idx >= 10 and ' ' or tostring(ctx.idx)
-              end,
-              highlight = 'BlinkCmpItemIdx' -- optional, only if you want to change its color
+    in
+    {
+      enable = true;
+      setupLspCapabilities = true;
+      package = blink-cmp-native;
+
+      settings = {
+        enabled.__raw = ''
+          function()
+            return not vim.list_contains({ 'sagarename' }, vim.bo.filetype)
+              and vim.b.completion ~= false
+          end'';
+
+        appearance.nerd_font_variant = "normal";
+        appearance.use_nvim_cmp_as_default = true;
+
+        completion.documentation.auto_show = false;
+        completion.list.max_items = 500;
+        completion.list.selection = {
+          auto_insert = false;
+          preselect = true;
+        };
+        completion.menu.draw.treesitter = [ "lsp" ]; # what is this doing?
+        # https://cmp.saghen.dev/configuration/reference.html#completion-menu-draw
+        completion.menu.draw.components.kind_icon.text.__raw = ''
+          function(ctx)
+            -- print(vim.inspect(ctx))
+            return ctx.kind_icon .. ctx.icon_gap
+          end
+        '';
+        completion.menu.draw.__raw = ''
+          {
+            columns = {
+              { "item_idx" },
+              { "kind_icon" },
+              -- :lua print(vim.inspect(vim.lsp.get_active_clients()[1].capabilities.textDocument.completion))
+              { "label", "label_description", gap = 1 },
+            },
+            components = {
+              item_idx = {
+                text = function(ctx)
+                  return ctx.idx == 10 and '0' or ctx.idx >= 10 and ' ' or tostring(ctx.idx)
+                end,
+                highlight = 'BlinkCmpItemIdx' -- optional, only if you want to change its color
+              }
             }
           }
-        }
-      '';
+        '';
 
-      fuzzy.prebuilt_binaries.download = false;
-      fuzzy.implementation = "prefer_rust_with_warning";
-      fuzzy.sorts.__raw = ''
-        {
-          "exact",
-          "score",
-          "sort_text",
-        }
-      '';
+        fuzzy.prebuilt_binaries.download = false;
+        fuzzy.implementation = "prefer_rust_with_warning";
+        fuzzy.sorts.__raw = ''
+          {
+            "exact",
+            "score",
+            "sort_text",
+          }
+        '';
 
-      signature.enabled = true;
-      signature.trigger.show_on_insert = true;
-      signature.window.show_documentation = true;
-      snippets.preset = "luasnip";
+        signature.enabled = true;
+        signature.trigger.show_on_insert = true;
+        signature.window.show_documentation = true;
+        snippets.preset = "luasnip";
 
-      completion.menu.border = "single";
-      completion.documentation.window.border = "single";
-      signature.window.border = "single";
+        completion.menu.border = "single";
+        completion.documentation.window.border = "single";
+        signature.window.border = "single";
+      };
     };
-  };
 }
