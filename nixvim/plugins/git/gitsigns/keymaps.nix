52a59023c6a9e05b19cad1c31e4b071d1a4c4a47
becknik <becknik@pm.me>
2025-07-11 15:32:18 +0200

    nixvim: revise gitsigns keymaps

--- nixvim/plugins/git/gitsigns/keymaps.nix
+++ nixvim/plugins/git/gitsigns/keymaps.nix
@@ -9,7 +9,6 @@
       { "<leader>hr", icon = "" },
       { "<leader>hR", icon = "" },
       { "<leader>hu", icon = "󰕍" },
-      { "<leader>hv", icon = "󰕜 " },
       { "<leader>hd", icon = " " },
       { "<leader>hD", icon = " " },
 
@@ -22,41 +21,40 @@
   keymaps = withDefaultKeymapOptions [
     {
       key = "<leader>hs";
-      action = "Gitsigns stage_hunk";
-      options.cmd = true;
+      action.__raw = "require'gitsigns'.stage_hunk";
       mode = mapToModeAbbr [ "normal" ];
-      options.desc = "Stage Hunk under cursor";
+      options.desc = "Stage Hunk under cursor (toggle)";
     }
     {
       key = "<leader>hs";
       action.__raw = "function() require'gitsigns'.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end";
       mode = mapToModeAbbr [ "visual_select" ];
-      options.desc = "Stage Hunk selection";
+      options.desc = "Stage Hunk selection (toggle)";
     }
     {
       key = "<leader>hS";
-      action = "Gitsigns stage_buffer";
-      options.cmd = true;
+      action.__raw = "require'gitsigns'.stage_hunk";
       options.desc = "Stage Hunks in buffer";
+    }
+    {
+      key = "<leader>hu";
+      action.__raw = "require'gitsigns'.undo_stage_hunk";
+      options.desc = "Undo stage Hunk";
     }
 
     {
-      key = "<leader>hu";
-      action = "Gitsigns undo_stage_hunk";
-      options.cmd = true;
-      options.desc = "Undo stage Hunk";
-    }
-    {
-      key = "<leader>hv";
-      action = "Gitsigns select_hunk";
-      options.cmd = true;
-      options.desc = "staged Hunk under cursor in Visual mode";
+      key = "ih";
+      action.__raw = "require'gitsigns'.select_hunk";
+      mode = mapToModeAbbr [
+        "operator_pending"
+        "visual_select"
+      ];
+      options.desc = "hunk";
     }
 
     {
       key = "<leader>hr"; # restore sadly doesn't exist in NeoGit
-      action = "Gitsigns reset_hunk";
-      options.cmd = true;
+      action.__raw = "require'gitsigns'.reset_hunk";
       mode = mapToModeAbbr [ "normal" ];
       options.desc = "Reset Hunk under cursor";
     }
@@ -68,21 +66,18 @@
     }
     {
       key = "<leader>hR";
-      action = "Gitsigns reset_buffer";
-      options.cmd = true;
+      action.__raw = "require'gitsigns'.reset_buffer";
       options.desc = "Reset Hunks in buffer";
     }
 
     {
       key = "<leader>hp";
-      action = "Gitsigns preview_hunk";
-      options.cmd = true;
+      action.__raw = "require'gitsigns'.preview_hunk";
       options.desc = "Preview Hunk diff under cursor";
     }
     {
       key = "<leader>hb";
-      action = "Gitsigns blame_line { full = true }";
-      options.cmd = true;
+      action.__raw = "function() require'gitsigns'.blame_line { full = true } end";
       options.desc = "Blame line";
     }
 
@@ -99,20 +94,17 @@
 
     {
       key = "<leader>tb";
-      action = "Gitsigns toggle_current_line_blame";
-      options.cmd = true;
+      action.__raw = "require'gitsigns'.toggle_current_line_blame";
       options.desc = "Toggle current line blame";
     }
     {
       key = "<leader>tx";
-      action = "Gitsigns toggle_deleted";
-      options.cmd = true;
+      action.__raw = "require'gitsigns'.toggle_deleted";
       options.desc = "Toggle visibility of deleted lines";
     }
     {
       key = "<leader>th";
-      action = "Gitsigns toggle_linehl";
-      options.cmd = true;
+      action.__raw = "require'gitsigns'.toggle_linehl";
       options.desc = "Toggle Hunk Highlighting";
     }
   ];
