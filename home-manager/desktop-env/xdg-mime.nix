8d55a5f2208e7fe187cbbfdcfc592a6c5dd13a3b
becknik <becknik@pm.me>
2025-08-01 17:09:51 +0200

    home-manager: fix xdg.mimeApps (has been broken forever)

--- home-manager/desktop-env/xdg-mime.nix
+++ home-manager/desktop-env/xdg-mime.nix
@@ -4,31 +4,38 @@
   xdg.configFile."mimeapps.list".force = true;
   #xdg.configFile."mimeapps.list".mutable = true;
 
-  # https://www.iana.org/assignments/media-types/media-types.xhtml
-  # TODO globbing isn't supported by default...
-  xdg.mimeApps =
-    let
-      assocations = {
-        "x-scheme-handler/sms" = "org.gnome.Shell.Extensions.GSConnect.desktop";
-        "x-scheme-handler/tel" = "org.gnome.Shell.Extensions.GSConnect.desktop";
+  xdg.mimeApps = {
+    enable = true;
 
-        "x-scheme-handler/http" = "librewolf.desktop;";
-        "x-scheme-handler/https" = "librewolf.desktop;";
-        "text/html" = "librewolf.desktop;";
-        "application/xhtml+xml" = "librewolf.desktop;";
+    # https://www.iana.org/assignments/media-types/media-types.xhtml
+    # xdg-mime query filetype ~/dl/2024.07.04_Rechnung_Kundennr_923412501.pdf
+    # xdg-mime query default 'application/pdf'
+    # gtk-launch org.kde.okular.desktop
+    defaultApplications = {
+      "text/plain" = [ "nvim.desktop" ];
 
-        "x-scheme-handler/mailto" = "thunderbird.desktop;";
+      "x-scheme-handler/sms" = [ "org.gnome.Shell.Extensions.GSConnect.desktop" ];
+      "x-scheme-handler/tel" = [ "org.gnome.Shell.Extensions.GSConnect.desktop" ];
 
-        "image/*" = "org.kde.gwenview.desktop;";
-        "video/*" = "vlc.desktop;";
-        "audio/*" = "vlc.desktop;";
-      };
-    in
-    {
-      enable = true;
-      associations.added = assocations;
-      defaultApplications = {
-        "text/plain" = "nvim.desktop";
-      } // assocations;
+      "x-scheme-handler/http" = [ "librewolf.desktop" ];
+      "x-scheme-handler/https" = [ "librewolf.desktop" ];
+      "application/xhtml+xml" = [ "librewolf.desktop" ];
+      "x-scheme-handler/about" = [ "librewolf.desktop" ];
+      "x-scheme-handler/unknown" = [ "librewolf.desktop" ];
+      "text/html" = [ "librewolf.desktop" ];
+
+      "x-scheme-handler/mailto" = [ "thunderbird.desktop" ];
+
+      "image/png" = [ "org.kde.gwenview.desktop" ];
+      "image/jpg" = [ "org.kde.gwenview.desktop" ];
+      "image/jepg" = [ "org.kde.gwenview.desktop" ];
+      "image/webp" = [ "org.kde.gwenview.desktop" ];
+      "image/svg+xml" = [ "org.kde.gwenview.desktop" ];
+
+      "video/*" = [ "vlc.desktop" ];
+      "audio/*" = [ "vlc.desktop" ];
+
+      "application/pdf" = [ "org.kde.okular.desktop" ];
     };
+  };
 }
