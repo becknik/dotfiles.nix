9b538199449505d1fd36c16bf4e171fa7201244b
becknik <becknik@pm.me>
2026-01-14 13:05:01 +0100

    nixvim: fix treesitter highlighting failing due to missing ecma import

--- nixvim/plugins/treesitter/default.nix
+++ nixvim/plugins/treesitter/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   imports = [
@@ -12,18 +12,32 @@
   plugins.treesitter = {
     enable = true;
 
+    highlight.enable = true;
+    indent.enable = true;
     folding.enable = true;
-    nixvimInjections = true;
+  };
 
-    settings = {
-      # grammars managed trough the plugin:
-      # auto install missing parsers when entering buffer
-      auto_install = true;
-      ensure_installed = [ ];
-      ignore_installed = [ ];
-
-      highlight.enable = true;
-      indent.enable = true;
+  # https://github.com/NixOS/nixpkgs/issues/478561
+  extraFiles = {
+    "queries/ecma/highlights.scm".source = pkgs.fetchurl {
+      url = "https://raw.githubusercontent.com/nvim-treesitter/nvim-treesitter/master/queries/ecma/highlights.scm";
+      sha256 = "sha256-N4NFR+uqnBYMrYfqvTg4fUcisbQNRLq1TY5x0f7/m54=";
+    };
+    "queries/ecma/indents.scm".source = pkgs.fetchurl {
+      url = "https://raw.githubusercontent.com/nvim-treesitter/nvim-treesitter/master/queries/ecma/indents.scm";
+      sha256 = "sha256-2gI78K/1/YG9+vt1Pz2jBBt8a6lT7wg4IaF6Yir20VU=";
+    };
+    "queries/ecma/injections.scm".source = pkgs.fetchurl {
+      url = "https://raw.githubusercontent.com/nvim-treesitter/nvim-treesitter/master/queries/ecma/injections.scm";
+      sha256 = "sha256-Dv/twrsEPTSic4RVOPmqae1f6BdkUS5WuS4aX2tbelw=";
+    };
+    "queries/ecma/folds.scm".source = pkgs.fetchurl {
+      url = "https://raw.githubusercontent.com/nvim-treesitter/nvim-treesitter/master/queries/ecma/folds.scm";
+      sha256 = "sha256-qXsY5St7yLcAde0aeNrRLnLl5a52m7IS2Cdhhqly3ZE=";
+    };
+    "queries/ecma/locals.scm".source = pkgs.fetchurl {
+      url = "https://raw.githubusercontent.com/nvim-treesitter/nvim-treesitter/master/queries/ecma/locals.scm";
+      sha256 = "sha256-ymzW5O4X3/T/w2B4D88uBKaNKDboesbkYDYgHh89ihU=";
     };
   };
 }
