51b730f14401e200b99cdcd8fa55e04f5085bb3c
becknik <becknik@pm.me>
2026-01-26 16:37:57 +0100

    nixvim: make `nvim-tree` window appear on the bottom-right

--- nixvim/plugins/nvim-tree.nix
+++ nixvim/plugins/nvim-tree.nix
@@ -21,17 +21,16 @@
               local screen_w = vim.opt.columns:get()
               local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
               local window_w = screen_w * 0.5
-              local window_h = screen_h * 0.8
+              local window_h = screen_h * 0.5
               local window_w_int = math.floor(window_w)
               local window_h_int = math.floor(window_h)
-              local center_x = (screen_w - window_w) / 2
-              local center_y = ((vim.opt.lines:get() - window_h) / 2)
-                - vim.opt.cmdheight:get()
+              local bottom_right_x = screen_w - window_w_int
+              local bottom_right_y = screen_h - window_h_int
               return {
                 border = 'rounded',
                 relative = 'editor',
-                row = center_y,
-                col = center_x,
+                row = bottom_right_y,
+                col = bottom_right_x,
                 width = window_w_int,
                 height = window_h_int,
               }
