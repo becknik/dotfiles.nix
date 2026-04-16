becknik/dotfiles.nix

commit f01a2105d15083676c3f6f4b316afe776225d40f

nixvim: fix some snippets (without trying them out)

nixvim/snippets/javascript/control-flow.lua
@@ -59,7 +59,7 @@ snippet_rec_elseif = function(start_index)
         } <>
       ]],
       {
-        i(1, "condition"),
+        i(1, "true"),
         i(2),
         d(3, snippet_rec_elseif, {}),
       }
@@ -75,13 +75,13 @@ end
 vim.list_extend(snippets, {
   s(
     { trig = "r", name = "return" },
-    fmta("return <>", {
+    fmta("return <>;", {
       c(
         1,
         utils.select_snippets(
           globals.functions,
           { "fn", "fni", "fnr", "fna", "fnia", "fnra" },
-          { i(1, "value") }
+          { i(1, "/* value */") }
         )
       ),
     })
@@ -96,7 +96,7 @@ vim.list_extend(snippets, {
         } <>
       ]],
       {
-        i(1, "condition"),
+        i(1, "true"),
         i(2),
         snippet_rec_elseif(3),
       }
@@ -117,7 +117,7 @@ vim.list_extend(snippets, {
         } <>
       ]],
       {
-        i(1, "condition"),
+        i(1),
         i(2),
         c(3, {
           t "",
@@ -137,22 +137,22 @@ vim.list_extend(snippets, {
     { trig = "te", name = "throw new Error" },
     fmta('throw new <>("<>")', {
       i(1, "Error"),
-      i(2, "message"),
+      i(2, 'message'),
     })
   ),
 
   s(
     { trig = "io", name = "instanceof" },
     fmta("<> instanceof <>", {
-      i(1, "value"),
-      i(2, "class"),
+      i(1, "/* value */"),
+      i(2, "/* Class */"),
     })
   ),
   s(
     { trig = "to", name = "typeof" },
     fmta("typeof <> === <>", {
-      i(1, "value"),
-      i(2, "type"),
+      i(1, "/* value */"),
+      i(2, "/* Type */"),
     })
   ),
 
@@ -168,9 +168,9 @@ vim.list_extend(snippets, {
       {
         i(1, "i"),
         l(l._1, 1),
-        i(2, "array.length"),
+        i(2, "[].length"),
         l(l._1 .. "++", 1),
-        i(3, "element"),
+        i(3, "/* element */"),
         dl(4, l._1:match "^(.*)%.", 2),
         l(l._1, 1),
         i(5),
@@ -205,7 +205,7 @@ vim.list_extend(snippets, {
         }
       ]],
       {
-        i(1, "condition"),
+        i(1, "true"),
         i(2),
       }
     )
@@ -220,7 +220,7 @@ vim.list_extend(snippets, {
       ]],
       {
         i(2),
-        i(1, "condition"),
+        i(1, "true"),
       }
     )
   ),

nixvim/snippets/typescriptreact.lua
@@ -104,7 +104,7 @@ vim.list_extend(snippets, {
         end, { 2 }, {
           user_args = { 2 },
         }),
-        i(0, "body"),
+        i(0, "// body"),
         f(function(args) return args[1][1] end, 3),
       }
     )
@@ -115,9 +115,9 @@ vim.list_extend(snippets, {
     l(l.TM_SELECTED_TEXT),
     t { "", "</>" },
   }),
-  s({ trig = "w&", name = "react && wrapper" }, {
+  s({ trig = "w7", name = "react && wrapper" }, {
     t { "{" },
-    i(1, "condition"),
+    i(1, "true"),
     t { " && (" },
     l(l.TM_SELECTED_TEXT),
     t { "", ")}" },
@@ -227,8 +227,8 @@ vim.list_extend(snippets, {
         })
       ]],
       {
-        i(2, ""),
-        i(1, ""),
+        i(2),
+        i(1),
       }
     )
   ),
@@ -245,7 +245,7 @@ vim.list_extend(snippets, {
         t "",
         fmt("<{}>", { i(1, "null") }),
       }),
-      i(2, ""),
+      i(2),
     })
   ),
   s(
@@ -262,7 +262,7 @@ vim.list_extend(snippets, {
   s(
     { trig = "uct", name = "react useContext" },
     fmta("const {<>} = useContext(<>);", {
-      i(1, ""),
+      i(1),
       i(2, "/* context */"),
     })
   ),
@@ -317,12 +317,12 @@ vim.list_extend(snippets, {
     { trig = "ac", name = "react Activity component" },
     fmt(
       [[
-        <Activity mode={{{}}}>
+        <Activity mode={{{}}  ? 'visible' : 'hidden'}>
           {}
         </Activity>
       ]],
       {
-        i(2, "isVisible ? 'visible' : 'hidden'"),
+        i(2, "true"),
         d(1, function(_, snip)
           if
             #snip.env.TM_SELECTED_TEXT == 0

nixvim/snippets/javascript/calls.lua
@@ -54,7 +54,7 @@ snippet_rec_ternary = function(_, _, _, call_number)
       }),
       fmta("\n\n<><> ? <> :<>", {
         f(function() return string.rep(" ", 2 * call_number) end),
-        i(1, "condition"),
+        i(1, "/* condition */"),
         i(2, "true"),
         d(3, snippet_rec_ternary, {}, { user_args = { call_number + 1 } }),
       }),
@@ -77,8 +77,8 @@ local snippets_calls = utils.insert_snippets(snippets, {
       return maybe_wrap_with_sn(
         params,
         fmta("new <>(<>)", {
-          i(1, "Type"),
-          i(2, "arguments"),
+          i(1, "Date"),
+          i(2),
         })
       )
     end,
@@ -89,7 +89,7 @@ local snippets_calls = utils.insert_snippets(snippets, {
       return maybe_wrap_with_sn(
         params,
         fmta("<> ? <> :<>", {
-          i(1, "condition"),
+          i(1, "true"),
           i(2, "true"),
           d(3, snippet_rec_ternary, {}, { user_args = { 1 } }),
         })
@@ -115,7 +115,7 @@ local snippets_calls = utils.insert_snippets(snippets, {
       return maybe_wrap_with_sn(
         params,
         fmta("Object.keys(<>)", {
-          i(1, "object"),
+          i(1),
         })
       )
     end,
@@ -127,7 +127,7 @@ local snippets_calls = utils.insert_snippets(snippets, {
       return maybe_wrap_with_sn(
         params,
         fmta("Object.values(<>)", {
-          i(1, "object"),
+          i(1),
         })
       )
     end,
@@ -138,7 +138,7 @@ local snippets_calls = utils.insert_snippets(snippets, {
       return maybe_wrap_with_sn(
         params,
         fmta("Object.entries(<>)", {
-          i(1, "object"),
+          i(1),
         })
       )
     end,
@@ -150,7 +150,7 @@ local snippets_calls = utils.insert_snippets(snippets, {
       return maybe_wrap_with_sn(
         params,
         fmta("JSON.stringify(<>)", {
-          i(1, "value"),
+          i(1),
         })
       )
     end,
@@ -161,7 +161,7 @@ local snippets_calls = utils.insert_snippets(snippets, {
       return maybe_wrap_with_sn(
         params,
         fmta("JSON.parse(<>)", {
-          i(1, "value"),
+          i(1),
         })
       )
     end,
@@ -191,20 +191,20 @@ vim.list_extend(snippets, {
   s(
     { trig = "apa", name = "await Promise.all" },
     fmta("await Promise.all(<>);", {
-      i(1, "items"),
+      i(1, "[]"),
     })
   ),
   s(
     { trig = "apad", name = "await Promise.all (destructured)" },
     fmta("const [<>] = await Promise.all(<>);", {
-      i(1, "items"),
-      i(2, "vars"),
+      i(2, "/* vars */"),
+      i(1),
     })
   ),
   s(
     { trig = "apam", name = "await Promise.all (map)" },
     fmta("await Promise.all(<>.map(<>));", {
-      i(1, "items"),
+      i(1, "[]"),
       c(2, utils.select_snippets(globals.functions, { "fni", "fnr" })),
     })
   ),

commit 6aaeef225efbd66eca2422f1c847f60d72e084ea

snippets: make them more useful and fix error introduced by f01a210

nixvim/snippets/javascript/control-flow.lua
@@ -160,7 +160,7 @@ vim.list_extend(snippets, {
     { trig = "fori", name = "for loop" },
     fmta(
       [[
-        for (let <> = 0; <> <= <>; <>) {
+        for (let <> = 0; <> <<= <>; <>) {
           const <> = <>[<>];
           <>
         }

nixvim/snippets/javascript/globals.lua
@@ -42,7 +42,7 @@ M.functions = utils.insert_snippets({ "javascript" }, {
 (<>) =>> {
   <>
 }
-]], { i(1, "params"), last_node() })
+]], { i(1), last_node() })
     end,
   },
   {
@@ -54,7 +54,7 @@ M.functions = utils.insert_snippets({ "javascript" }, {
 function (<>) {
   <>
 }
-]], { i(1, "params"), last_node() })
+]], { i(1), last_node() })
     end,
   },
   {
@@ -64,7 +64,7 @@ function (<>) {
 
       return fmta(
         (params.async and "async " or "") .. "(<>) =>> <>",
-        { i(1, "params"), last_node() }
+        { i(1), last_node() }
       )
     end,
   },
@@ -81,7 +81,7 @@ function (<>) {
   return <>;
 }
 ]], {
-        i(1, "params"),
+        i(1),
         second_node(),
         d(3, ts_utils.last_var_declaration_in_function, { 2 }),
       })

nixvim/snippets/typescriptreact.lua
@@ -222,13 +222,15 @@ vim.list_extend(snippets, {
     { trig = "um", name = "react useMemo" },
     fmta(
       [[
-        const {<>} = useMemo(() =>> {
-          <>
-        })
+        const <> = useMemo(<>, [<>])
       ]],
       {
-        i(2),
-        i(1),
+        c(1, {
+          fmta("{<>}", { i(1) }),
+          t "values",
+        }),
+        c(2, utils.select_snippets(globals_js.functions, { "fnr", "fni" })),
+        i(3),
       }
     )
   ),
@@ -262,8 +264,8 @@ vim.list_extend(snippets, {
   s(
     { trig = "uct", name = "react useContext" },
     fmta("const {<>} = useContext(<>);", {
-      i(1, ""),
-      i(2, "/* context */"),
+      i(2),
+      i(1, "/* Context */"),
     })
   ),
   s(