2f309425b618ddb7dcb6a42de0c008c211da4215
becknik <becknik@pm.me>
2026-05-11 12:16:18 +0200

    home-manager: remove alacritty

--- home-manager/desktop-env/shell/terminal-emulators.nix
+++ home-manager/desktop-env/shell/terminal-emulators.nix
@@ -2,55 +2,6 @@
 
 {
   programs = {
-    alacritty = {
-      enable = true;
-
-      settings = {
-        window = {
-          dimensions = {
-            columns = 132;
-            lines = 43;
-          };
-          decorations = "Full"; # "Transparent" "Buttonless" "None" "Full"
-          decorations_theme_variant = "Dark"; # "Light" "None"
-          opacity = 0.9;
-          blur = true;
-          dynamic_title = false;
-          resize_increments = true; # Prefer resizing window by discrete steps equal to cell dimensions.
-          # option_as_alt
-        };
-        scrolling = {
-          history = 25000;
-          multiplier = 3; # lines scrolled per increment
-        };
-        font = {
-          normal = {
-            family = "Fira Code Nerd Font Mono";
-            style = "Retina";
-          }; # ligatures aren't supported tho
-          # https://github.com/alacritty/alacritty/pull/5696
-          bold = {
-            style = "SemiBold";
-          };
-        };
-        colors.transparent_background_colors = false; # default
-        bell.duration = 200;
-        terminal.osc52 = "OnlyCopy"; # "Disabled" | "OnlyCopy" | "OnlyPaste" | "CopyPaste"
-        mouse.hide_when_typing = false;
-        # TODO `= true` doesn't bings up the mouse visibility again after stopping typing and moving the mouse
-        keyboard.bindings = [
-          {
-            mods = "Control|Shift";
-            key = "N";
-            action = "CreateNewWindow";
-          }
-        ];
-        env = {
-          TERM = "xterm-256color";
-        };
-      };
-    };
-
     kitty = {
       enable = true;
       font = {
@@ -61,6 +12,8 @@
       };
 
       settings = {
+        modify_font = "cell_height +2px";
+
         scrollback_lines = 5000;
         # accessible when using a separate page for scrollback
         # toggle with kitty_mod + h
@@ -85,6 +38,5 @@
         update_check_interval = 0;
       };
     };
-
   };
 }
