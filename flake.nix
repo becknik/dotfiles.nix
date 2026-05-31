49c3fd6865975c8af27a74f9df33252ab8556206
becknik <becknik@pm.me>
2026-05-31 12:12:38 +0200

    update to 26.05

--- flake.nix
+++ flake.nix
@@ -4,8 +4,8 @@
   inputs = {
     flake-input-patcher.url = "github:jfly/flake-input-patcher";
 
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-    nixpkgs-darwin.url = "github:NixOS/nixpkgs/nixpkgs-25.11-darwin";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    nixpkgs-darwin.url = "github:NixOS/nixpkgs/nixpkgs-26.05-darwin";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
@@ -29,12 +29,12 @@
     };
 
     darwin = {
-      url = "github:LnL7/nix-darwin/nix-darwin-25.11";
+      url = "github:LnL7/nix-darwin/nix-darwin-26.05";
       inputs.nixpkgs.follows = "nixpkgs-darwin";
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     sops-nix = {
@@ -110,7 +110,7 @@
       ];
       forAllSystems = nixpkgs.lib.genAttrs systems;
 
-      stateVersion = "25.11";
+      stateVersion = "26.05";
 
       # Necessary e.g. for NixOS `config.system.autoUpgrade.flake` with `--commit-lockfile`
       mkFlakeDir =
